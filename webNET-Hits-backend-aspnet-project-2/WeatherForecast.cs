48b004c27cecd19fe018b3f2b05f0a0858744c3f
alyoneek <alyonta03@mail.ru>
2022-11-29 23:23:02 +0700

    Initial commit

---
+++ webNET-Hits-backend-aspnet-project-2/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace webNET_Hits_backend_aspnet_project_2
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
